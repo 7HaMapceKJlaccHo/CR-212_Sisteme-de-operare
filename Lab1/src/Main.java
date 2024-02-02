7b22005e305183cc278db28977ad6ae9962e62d2
LaiCristian <laicristian123@gmail.com>
2024-02-02 18:21:58 +0200

    created main file

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -1,5 +1,38 @@
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
+public class Main extends JFrame {
+
+    public Main() {
+        setTitle("Laborator 1");
+
+        setLayout(new GridLayout(1, 3));
+
+        JPanel sectiune3 = createSection("Cristi");
+        JPanel sectiune2 = createSection("Roman");
+        JPanel sectiune1 = createSection("Virgiliu");
+
+        add(sectiune1);
+        add(sectiune2);
+        add(sectiune3);
+
+        setSize(600, 200);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setVisible(true);
+    }
+
+    private JPanel createSection(String labelText) {
+        JPanel sectionPanel = new JPanel(new FlowLayout());
+
+        Border border = BorderFactory.createTitledBorder(labelText);
+        sectionPanel.setBorder(border);
+        return sectionPanel;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Main::new);
     }
 }
