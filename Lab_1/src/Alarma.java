56c350b174b9aa44d6de9607ac72188ff7b9aa7c
arina <arinacaraus@gmail.com>
2024-02-08 23:55:34 +0200

    laboratorul_1

---
+++ Lab_1/src/Alarma.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.util.Timer;
+import java.util.TimerTask;
+
+class Alarma extends TimerTask {
+    public String mesaj ;
+    JFrame frame;
+    public Alarma(String mesaj) {
+        this.mesaj = mesaj ;
+    }
+
+    public void run() {
+
+
+
+        System.out.println( mesaj );
+        JOptionPane.showMessageDialog(frame, mesaj);
+    }
+}
