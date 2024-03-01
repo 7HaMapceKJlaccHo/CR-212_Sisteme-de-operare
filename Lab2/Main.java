2a41ca4cfc2f3801da0dc472f19629394ff594e7
arina <arinacaraus@gmail.com>
2024-03-01 17:43:36 +0200

    Arina's part

--- Lab2/Main.java
+++ Lab2/Main.java
@@ -1,3 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
